090f243d6bee49e8609244a9cb4d1c448bd3ab11
N1chegons <krikignatenko@gmail.com>
2025-09-16 14:24:04 +0500

    third update

---
+++ src/trade/models.py
@@ -0,0 +1,19 @@
+import enum
+from datetime import datetime
+
+from sqlalchemy.orm import Mapped
+from sqlalchemy.testing.schema import mapped_column
+from src.database import Base
+
+
+class Direction(enum.Enum):
+    long = "Long"
+    short = "Short"
+
+class Trade(Base):
+    __tablename__ = "trades"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    ticket: Mapped[str]
+    direction: Mapped[Direction]
+    date: Mapped[datetime.date]
